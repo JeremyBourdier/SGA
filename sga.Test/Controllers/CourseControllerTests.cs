9b6b44b1036192451db0af5c7e65eecacf83228e
Jeremy Bourdier Estrella <20211917@itla.edu.do>
2025-02-26 17:12:41 -0300

    Pruena unitaria del User a la base de datos exitosa

--- sga.Test/Controllers/CourseControllerTests.cs
+++ sga.Test/Controllers/CourseControllerTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using sga.AcademicService.Controllers;
+using sga.AcademicService.DTOs;
+using sga.AcademicService.Services.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace sga.Test.Controllers
 {
-    internal class CourseControllerTests
+    public class CourseControllerTests
     {
+
     }
 }
